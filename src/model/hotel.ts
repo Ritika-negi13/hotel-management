00b1f9a7d1f3323ad223ddc7abbb4b09f9dac034
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-06 14:49:54 +0530

    class hotel added

---
+++ src/model/hotel.ts
@@ -0,0 +1,10 @@
+export class hotel{
+    id!:string;
+    name!:string;
+    location!:string;
+    constructor(name:string,id:string,location:string){
+        this.name=name;
+        this.id=id;
+        this.location=location;
+    }
+}
