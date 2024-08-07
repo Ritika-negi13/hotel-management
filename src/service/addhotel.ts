7dc5d73a082e4892e5d2fe3609c8f7b33c6b31f4
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-08-07 14:15:44 +0530

    addHotel.ts file added

--- src/service/addhotel.ts
+++ src/service/addhotel.ts
@@ -1,12 +1,18 @@
 import {hotel} from '../model/hotel';
-export let hotels:hotel[] = [];
+export let hotels:hotel[] = [
+    {
+        name:'Hotel Tulip Inn',
+        id:'234',
+        location:'Banglore',
+        isAvailable:true,
+    }
+];
 
 export function addHotel(h1:hotel){
     hotels.push(h1);
+    console.log(hotels);
 }
-
-let h1 = new hotel('Tulip', '1', 'Banglore');
-addHotel(h1);
-
-
-console.log(hotels);
+export function showHotels()
+{
+    console.log(hotels)
+}
